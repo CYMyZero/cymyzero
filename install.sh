a8db744462df38906371e4dcfeef8a3b3edf3ed3
CYMyZero <50820670+CYMyZero@users.noreply.github.com>
2020-10-22 11:36:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1065,7 +1065,7 @@ while :; do
 		echo -e "$yellow 温馨提示.. 本地安装已启用 ..$none"
 		echo
 	fi
-	read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" choose
+	read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" 1
 	case 1 in
 	1)
 		install
